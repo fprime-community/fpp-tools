5c62f6ca80ab2787dcc445ddead6703f1d94a2e5
Andrei Tumbar <andreitumbar@gmail.com>
2026-09-12 07:48:42 -0700

    Format

--- fpp_python/src/diagnostics.rs
+++ fpp_python/src/diagnostics.rs
@@ -69,12 +69,7 @@ impl fpp_core::DiagnosticEmitter for SharedEmitter {
         let children = diagnostic
             .children
             .iter()
-            .map(|c| {
-                (
-                    c.message.clone(),
-                    c.span.as_ref().map(|s| loc_of(s)),
-                )
-            })
+            .map(|c| (c.message.clone(), c.span.as_ref().map(|s| loc_of(s))))
             .collect();
         self.diags.lock().unwrap().push(OwnedDiagnostic {
             level: level_str(diagnostic.level),
